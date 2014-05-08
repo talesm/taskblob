b5094ca05ee950fb5f6b6ca31d927c1f96bec94e
TalesM <tales.miranda84@gmail.com>
2014-05-08 00:02:49 -0300

    Detected bug at exportar. Missing callback at importar.

--- import.js
+++ import.js
@@ -2,4 +2,13 @@
  * 
  */
 $(function() {
+	$('.showImport').click(function(){
+		createModal('.importDialog', {closeButton: true});
+	});
+	$('.modal').on('click', '.textImport', function () {
+		var selectedFile = $('.importFile').get(0).files[0];
+		var reader = new FileReader();
+		alert(selectedFile.name);
+		alert(reader.readAsText(selectedFile));
+	});
 });
