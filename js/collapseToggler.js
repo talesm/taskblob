93a41ac352b86fcacafdafe82bb13e8c30bdbab5
TalesM <tales.miranda84@gmail.com>
2014-05-24 12:39:56 -0300

    Added feture to expand/collapse group with dblclick.

--- js/collapseToggler.js
+++ js/collapseToggler.js
@@ -9,7 +9,16 @@ $(function() {
 		var $item = $('#' + makeItemName(path));
 		$item.toggleClass('collapsed');
 		$this.closest('.popMenu').hide('fast');
-//		$this.closest('.itemButtons').children().toggle('collapsed')
+		$this.children('span .ui-icon-folder-open').toggle();
+	});
+	$('.viewGroup').on('dblclick', '.taskName', function(ev){
+		var $this=$(this);
+		var path = $this.closest('.item').attr('data-itemid').split('.');
+		var item=tasks.get(path);
+		if(!item || !item.subTasks)
+			return;
+		var $item = $('#' + makeItemName(path));
+		$item.toggleClass('collapsed');
 		$this.children('span .ui-icon-folder-open').toggle();
 	});
 });
