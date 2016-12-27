8124e6c62f4e74f21209b998308a4f81df121670
talesm <tales.miranda84@gmail.com>
2016-12-27 19:05:38 -0200

    Simplified GanttItemName.

--- src/ui/GanttItemName.js
+++ src/ui/GanttItemName.js
@@ -1,63 +1,54 @@
 import React from 'react';
 
-export default class extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {editing:false};
+export default function(props) {
+  let name;
+  const submit = (ev) => {
+    if(props.onSubmit){
+      props.onSubmit(ev);
+    }
   }
-
-  render(){
-    let name;
-    const submit = (ev) => {
-      this.setState({editing:false});
-      if(this.props.onSubmit){
-        this.props.onSubmit(ev);
-      }
+  const reset = (ev) => {
+    if(props.onReset){
+      props.onReset(ev);
     }
-    const reset = (ev) => {
-      this.setState({editing:false});
-      if(this.props.onReset){
-        this.props.onReset(ev);
-      }
+  }
+  const keydown = (ev) => {
+    switch (ev.key) {
+      case 'ArrowDown':
+      case 'ArrowUp':
+      case 'Enter':
+        submit(ev);
+        break;
+      case 'Escape':
+        reset(ev);
+        break;
+      default:
+        break;
     }
-    const keydown = (ev) => {
-      switch (ev.key) {
-        case 'ArrowDown':
-        case 'ArrowUp':
-        case 'Enter':
-          submit(ev);
-          break;
-        case 'Escape':
-          reset(ev);
-          break;
-        default:
-          break;
-      }
-    }
-    if(this.props.selected){
-      name = (
-        <input autoFocus
-          className="name"
-          value={this.props.children}
-          onChange={this.props.onChange}
-          onKeyDown={keydown}
-          onReset={this.props.onReset}
-        />
-      );
-    } else {
-      name = <span className="name">{this.props.children}</span>;
-    }
-    const classes = ['taskName'];
-    if(this.props.selected){
-      classes.push('selected')
-    }
-    if(this.props.closed){
-      classes.push('closed')
-    }
-    return (
-      <span onBlur={reset} className={classes.join(' ')} title={this.props.children} onClick={()=>this.setState({editing:true})}>
-        {name}
-      </span>
+  }
+  if(props.selected){
+    name = (
+      <input autoFocus
+        className="name"
+        value={props.children}
+        onChange={props.onChange}
+        onKeyDown={keydown}
+        onReset={props.onReset}
+      />
     );
+  } else {
+    name = <span className="name">{props.children}</span>;
   }
+  const classes = ['taskName'];
+  if(props.selected){
+    classes.push('selected')
+  }
+  if(props.closed){
+    classes.push('closed')
+  }
+  return (
+    <span onBlur={reset} className={classes.join(' ')} title={props.children}>
+      {name}
+    </span>
+  );
 }
