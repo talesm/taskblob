d83fe9b4cd0ccae86b591860202fa217a8b93557
talesm <tales.miranda84@gmail.com>
2016-12-28 17:24:48 -0200

    Added a way to handle focus changes

--- src/ui/GanttGraph.js
+++ src/ui/GanttGraph.js
@@ -19,25 +19,39 @@ export default class extends React.Component{
         item={item}
         selected={this.state.selected === index}
         onSubmit={this.props.onEditItem}
-        onClick={()=>this.setState({selected: index})}
+        onClick={this.onClickItem.bind(this, index)}
       >
         {item.name}
       </ItemController>
     ));
+    const length = this.props.items.length;
     return (
-      <div className="viewGroup" onKeyDown={this.onKeyDown}>
+      <div
+        className="viewGroup"
+        onKeyDown={this.onKeyDown}
+        onClick={this.onClick}
+      >
         <TimeLabel min="0" max="15" interval="5" unit="h">{this.props.children}</TimeLabel>
         {items}
         <ItemController
           template={GanttItem}
           className="placeholder"
           onSubmit={this.props.onInsertItem}
-          selected={this.state.selected === this.props.items.length}
-          onClick={()=>this.setState({selected: this.props.items.length})}
+          selected={this.state.selected === length}
+          onClick={this.onClickItem.bind(this, length)}
           placeholder="Add New Task"
         />
       </div>
     );
+  }
+
+  onClickItem(index, ev){
+    this.setState({selected: index});
+    ev.stopPropagation();
+  }
+
+  onClick = (ev) => {
+    this.setState({selected: null});
   }
 
   onKeyDown = (ev) => {
