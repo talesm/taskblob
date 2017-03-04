bc156503295f4e61b68af99def05f0811b86c5ad
talesm <tales.miranda84@gmail.com>
2017-03-03 22:38:46 -0300

    For #2, Now persisting project data on localStorage.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,40 @@ import React from 'react';
 import MainView from './ui/MainView';
 import ProjectController from './control/ProjectController';
 import './App.css';
+import Project from './model/Project';
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {project: this.load()};
+    window.onbeforeunload = function(e) {
+      this.persist();
+    }.bind(this);
+    setInterval(this.persist, 2000)
+  }
+
+  componentDidMount() { }
+
+  componentWillUnmount() { }
+
   render() {
-    return <ProjectController template={MainView}/>;
+    return <ProjectController template={MainView} project={this.state.project}/>;
+  }
+
+  persist = () => {
+    localStorage.setItem('taskBlobProject', JSON.stringify(this.state.project.dry()));
+  }
+
+  load = () => {
+    try {
+      let driedProject = JSON.parse(localStorage.getItem('taskBlobProject'));
+      if(driedProject){
+        return Project.wet(driedProject);
+      }
+    } catch(ex){
+      console.error(ex);
+    }
+    return new Project('Project', '');
   }
 }
 
--- src/control/ProjectController.js
+++ src/control/ProjectController.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import Project from '../model/Project';
 import Task from '../model/Task';
 
 export default class extends React.Component {
   constructor(props) {
     super(props);
-    this.project = new Project('Test1', 'Test1');
-    this.project.addKid(new Task(1, 'Test1', 'description', 8, 4, false));
-    this.project.addKid(new Task(2, 'Test2', 'description', 8, 12, false));
-    this.project.addKid(new Task(3, 'Test3', 'description', 8, 4, true));
-    this.project.addKid(new Task(4, 'Test4', 'description', 8, 12, true));
-    this.state = { project: this.project.toArray() };
+    this.state = { project: props.project.toArray() };
   }
 
   render() {
@@ -28,17 +22,17 @@ export default class extends React.Component {
     if(!item.name){
       return;
     }
-    const task = new Task(this.project.size(), item.name);
-    this.project.addKid(task);
-    this.setState({ project: this.project.toArray() });
+    const task = new Task(this.props.project.size(), item.name);
+    this.props.project.addKid(task);
+    this.setState({ project: this.props.project.toArray() });
   }
 
   editItem = (item) => {
-    this.setState({ project: this.project.toArray() });
+    this.setState({ project: this.props.project.toArray() });
   }
 
   eraseItem = (item) => {
-    this.project.removeKid(item);
-    this.setState({ project: this.project.toArray() });
+    this.props.project.removeKid(item);
+    this.setState({ project: this.props.project.toArray() });
   }
 }
